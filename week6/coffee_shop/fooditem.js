c3a3a376dbfcfa13e8161db902d7878adc75bbf3
Rtheiking1287 <100746144+Rtheiking1287@users.noreply.github.com>
2022-06-08 18:30:25 -0700

    Add files via upload

---
+++ week6/coffee_shop/fooditem.js
@@ -0,0 +1,100 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Coffee',
+    category : 'drinks',
+    price: 2,
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Tea',
+    category : 'drinks',
+    price: 2,
+    img: 'https://www.verywellhealth.com/thmb/ZGs7ufUg-ohsUzQrZZ_Lfbfavns=/1989x1492/smart/filters:no_upscale()/GettyImages-693893647-588d21e413dd411cb1f2b0a0ea3e02da.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Juice',
+    category : 'drinks',
+    price: 3,
+    img: 'https://images.eatthismuch.com/img/1468_J%20Forque_540e76c0-2df0-4978-9f0b-b8dc1bb6bf52.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Soda',
+    category : 'drinks',
+    price: 3,
+    img: 'https://www.eatthis.com/wp-content/uploads/sites/4/2021/06/soda-2.jpg?quality=82&strip=all&w=1200',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Burger',
+    category : 'entrees',
+    price: 5,
+    img: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F07%2F13%2FUltimate-Veggie-Burgers-FT-Recipe-0821.jpg&q=60',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Sandwich',
+    category : 'entrees',
+    price: 4,
+    img: 'https://www.jennieo.com/wp-content/uploads/2019/11/image-recipe_cracked-pepper-turkey-sandwich.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Hot Dog',
+    category : 'entrees',
+    price: 3,
+    img: 'https://leitesculinaria.com/wp-content/uploads/2021/05/perfect-hot-dog.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Chicken Tenders',
+    category : 'entrees',
+    price: 3,
+    img: 'https://cdn.shopify.com/s/files/1/0580/3245/5858/products/tenders-new-082521.jpg?v=1629903096&width=1080',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Cake',
+    category : 'desserts',
+    price: 3,
+    img: 'https://www.cookingclassy.com/wp-content/uploads/2019/10/chocolate-cake-3.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Pie',
+    category : 'desserts',
+    price: 3,
+    img: 'https://media.istockphoto.com/photos/slice-of-apple-pie-on-a-plate-isolalted-on-a-white-background-picture-id184350005?k=20&m=184350005&s=612x612&w=0&h=feNglQ4LghHDn27_zZGWayvXlkyayqySGibL7j7D2Vo=',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Ice Cream',
+    category : 'desserts',
+    price: 2,
+    img: 'https://media.istockphoto.com/photos/ice-cream-scoops-different-flavor-strawberry-chocolate-creamy-in-picture-id1162964737?k=20&m=1162964737&s=612x612&w=0&h=SwdaPnrrztExV-3nyCbTESd7HeSyZ6Dvmk8OGNzlYsc=',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Fudge Brownies',
+    category : 'desserts',
+    price: 2,
+    img: 'https://bakerbettie.com/wp-content/uploads/2013/05/easy-brownie-recipe-1-700-x-700-px-1.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
